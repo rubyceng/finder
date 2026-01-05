4e5815f537940c12058a02d7fea828200a7c8724
Ruby Ceng <rubyceng0326@gmail.com>
2026-01-05 23:43:08 +0800

    feat: application init

---
+++ lib/main.dart
@@ -0,0 +1,27 @@
+import 'package:flutter/cupertino.dart';
+
+void main() => runApp(const App());
+
+class App extends StatelessWidget {
+  const App({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return const CupertinoApp(
+      debugShowCheckedModeBanner: false,
+      home: HomePage(),
+    );
+  }
+}
+
+class HomePage extends StatelessWidget {
+  const HomePage({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return const CupertinoPageScaffold(
+      navigationBar: CupertinoNavigationBar(middle: Text('Hello')),
+      child: Center(child: Text('It works!')),
+    );
+  }
+}
